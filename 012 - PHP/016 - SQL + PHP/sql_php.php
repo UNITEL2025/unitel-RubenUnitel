61643c0f4e4816a7cb4315ea0996eb6532186c12
RubenUnitel <ruben.unitel.talavera@gmail.com>
2025-07-24 14:21:30 +0200

    24/07/2025

---
+++ 012 - PHP/016 - SQL + PHP/sql_php.php
@@ -0,0 +1,28 @@
+<?php
+
+    # IMPORTANTE! Activar servidor sql
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    
+    
+    // Create connection
+    /*$conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";*/
+
+    /*$db = "academia";
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=".$db, $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }*/
+?>
